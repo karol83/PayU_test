3dd3fc64d7819474ec69ca9e25c42ba0604ad900
Karol Drazynski <karol.drazynski@gmail.com>
2019-04-04 14:53:41 +0200

    All hosts allowed for testing

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,8 +8,9 @@ import django_heroku
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS',
-                         default=['django-env.qikmc6sunc.us-west-2.elasticbeanstalk.com'])
+# ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS',
+#                          default=['django-env.qikmc6sunc.us-west-2.elasticbeanstalk.com'])
+ALLOWED_HOSTS = ['*']
 
 # DATABASES
 # ------------------------------------------------------------------------------
