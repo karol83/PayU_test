e7aaf7d90d73230ac3a12780e690f24ed5ee104d
Karol Drazynski <karol.drazynski@gmail.com>
2019-04-04 13:44:03 +0200

    removed file logger

--- config/settings/local.py
+++ config/settings/local.py
@@ -72,11 +72,7 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'app_debug.log',
-        },
+
         'console': {
             'class': 'logging.StreamHandler',
         },
